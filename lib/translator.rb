256d123bbbd311e9f72e12b1579334cbcf97537a
Irvin Natt <ipnatt@gmail.com>
2020-11-09 01:53:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,28 +4,10 @@ require "pry"
 
 def load_library(path_file)
   emoticons = YAML.load_file("./lib/emoticons.yml")
-  file_path = {}
-  file_path[:get_meaning] = {}
-  file_path[:get_emoticon] = {}
-  
-  emoticons.each do |word, pair|
-    japanese => pair[1]
-    english => pair[0]
-    
-    file_path[:get_meaning][japanese] = word
-    file_path[:get_emoticon][english] = japanese
-  end
-  file_path
 end
     
 
-def get_japanese_emoticon(path, emoticon)
-  file_path = load_library(path_file)
-  if file_path[:get_emoticon][emoticon]
-    return file_path[:get_emoticon][emoticon]
-  else
-    return "Sorry, that emoticon was not found"
-  end
+def get_japanese_emoticon
 end
 
 def get_english_meaning
