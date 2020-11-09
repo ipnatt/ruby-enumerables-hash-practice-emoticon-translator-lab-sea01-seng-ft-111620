8492d1d6b6d8bb7c18bdf7c0d6f069b7ac83920c
Irvin Natt <ipnatt@gmail.com>
2020-11-09 02:15:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,7 @@ end
 
 def get_japanese_emoticon(path_file, emoticon)
   emoticons_hash = load_library(path_file)
-  japanese_emoticon
+  japanese_emoticon = emoticons_hash
 end
 
 def get_english_meaning
