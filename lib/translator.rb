4a872d4476c8e7e58a18e8018f4d48e1980f22d6
Irvin Natt <ipnatt@gmail.com>
2020-11-09 01:36:37 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,7 @@ require "yaml"
 require "pry"
 
 
-def load_library(file_path)
+def load_library(path_file)
   emoticons = YAML.load_file("./lib/emoticons.yml")
   new_hash = {"get_meaning" => {}, "get_emoticon" => {}}
   emoticons.each do |key, value|
