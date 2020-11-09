d120120518db20734b99019c6911fcd7a138aa44
Irvin Natt <ipnatt@gmail.com>
2020-11-09 01:52:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,7 +24,7 @@ def get_japanese_emoticon(path, emoticon)
   if file_path[:get_emoticon][emoticon]
     return file_path[:get_emoticon][emoticon]
   else
-    return "Sorry"
+    return "Sorry, that emoticon was not found"
   
 end
 
