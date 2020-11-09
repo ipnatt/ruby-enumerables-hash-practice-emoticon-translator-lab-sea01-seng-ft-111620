5efdb2936ad42e47bda323c1bb96d2e2f7e6c53c
Irvin Natt <ipnatt@gmail.com>
2020-11-09 02:18:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,7 @@ end
 def get_japanese_emoticon(path_file, emoticon)
   emoticons_hash = load_library(path_file)
   japanese_emoticon = emoticons_hash["get_emoticon"][emoticon]
-  japanese_emoticon ? japanese_emoticon: "Sorry, that emoticon was not known!"
+  japanese_emoticon ? japanese_emoticon: "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning
