2600477adf997198654ff15bc657d5275f36437b
Irvin Natt <ipnatt@gmail.com>
2020-11-09 02:02:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,7 @@ def load_library(path_file)
   new_hash[:get_emoticon] = {}
   emoticons.each do |word, emoticon|
     new_hash[:get_meaning][emoticon[1]] = word
-    new_hash[:get_emoticon][emoticon[0]] = 
+    new_hash[:get_emoticon][emoticon[0]] = emoticon[1]
 
   
 end
