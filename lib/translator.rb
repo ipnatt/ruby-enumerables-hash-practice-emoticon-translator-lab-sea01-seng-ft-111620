44ef2e5490efd8f4d7924a0f88230c1cde5731e6
Irvin Natt <ipnatt@gmail.com>
2020-11-09 06:58:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,17 +2,7 @@ require "yaml"
 require "pry"
 
 
-def load_library(path_file)
-  emoticons = YAML.load_file("./lib/emoticons.yml")
-  emoticons_hash = { "get_meaning" => {}, "get_emoticon" => {} }
-  emoticons.each do |meaning, value|
-    english = value[0]
-    japanese = value[1]
-    emoticons_hash["get_meaning"][japanese] = meaning
-    emoticons_hash["get_emoticon"][english] = japanese
-  end
-  emoticons_hash
-end
+
     
 
 def get_japanese_emoticon(path_file, emoticon)
