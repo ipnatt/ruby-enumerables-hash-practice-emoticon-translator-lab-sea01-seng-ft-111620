79f6033c3798165544e38ba6b2fac0180a4c3b81
Irvin Natt <ipnatt@gmail.com>
2020-11-09 01:45:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,8 +9,8 @@ def load_library(path_file)
   file_path[:get_emoticon] = {}
   
   emoticons.each do |key, value|
-    japanese => value[0]
-    english => value[1]
+    japanese => value[1]
+    english => value[0]
     
     file_path[:get_meaning][japanese] = key
     file_path[:get_emoticon][english] = japanese
